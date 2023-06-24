eadf9df395d1e92b1b8bd01676d8f3e33e2fa78b
LevKruglyak <lev.kruglyak2014@gmail.com>
2023-06-24 14:04:30 +0200

    btree

--- clearned_core/src/classical/layer.rs
+++ clearned_core/src/classical/layer.rs
@@ -1,17 +1,118 @@
-use std::ops::Deref;
+use std::{ops::Deref, path::Path};
 
-use super::node::BTreeNode;
-use crate::{Key, NodeLayer, Value};
+use super::node::{BTreeNode, KeyPtr};
+use crate::{
+    path_with_extension,
+    search::{lower_bound, OptimalSearch, Search},
+    ApproxPos, InternalLayer, InternalLayerBuild, Key, NodeLayer, Result,
+};
 use mmap_buffer::Buffer;
 
-pub struct BTreeLayer<K: Key, V: Value, const FANOUT: usize> {
-    nodes: Buffer<BTreeNode<K, V, FANOUT>>,
+pub struct BTreeLayer<K: Key, const FANOUT: usize> {
+    nodes: Buffer<BTreeNode<K, usize, FANOUT>>,
 }
 
-impl<K: Key, V: Value, const FANOUT: usize> NodeLayer<K> for BTreeLayer<K, V, FANOUT> {
-    type Node = BTreeNode<K, V, FANOUT>;
+impl<K: Key, const FANOUT: usize> NodeLayer<K> for BTreeLayer<K, FANOUT> {
+    type Node = BTreeNode<K, usize, FANOUT>;
 
     fn nodes(&self) -> &[Self::Node] {
         self.nodes.deref()
+    }
+}
+
+impl<K: Key, const FANOUT: usize> InternalLayer<K> for BTreeLayer<K, FANOUT> {
+    fn search(&self, key: &K, range: ApproxPos) -> ApproxPos {
+        // Small optimization for exact positions
+        let node = if range.lo == range.hi - 1 {
+            self.nodes[range.lo]
+        } else {
+            self.nodes[lower_bound(OptimalSearch::search_by_key_with_offset(
+                &self.nodes[range.lo..range.hi],
+                key,
+                range.lo,
+            ))]
+        };
+
+        let ptr = node.search(key);
+
+        ApproxPos {
+            lo: ptr,
+            hi: ptr + 1,
+        }
+    }
+}
+
+impl<K: Key, const FANOUT: usize> InternalLayerBuild<K> for BTreeLayer<K, FANOUT> {
+    fn build(base: Vec<K>) -> Self
+    where
+        Self: Sized,
+    {
+        let data: Vec<KeyPtr<K, usize>> = base
+            .iter()
+            .copied()
+            .enumerate()
+            .map(|(ptr, min)| KeyPtr::new(min, ptr))
+            .collect();
+
+        let capacity = data.len() / FANOUT + 2;
+        let nodes = Buffer::new_in_memory(capacity);
+
+        Self::build_internal(&data, nodes)
+    }
+
+    fn build_on_disk(base: Vec<K>, path: impl AsRef<Path>) -> Result<Self>
+    where
+        Self: Sized,
+    {
+        let data: Vec<KeyPtr<K, usize>> = base
+            .iter()
+            .copied()
+            .enumerate()
+            .map(|(ptr, min)| KeyPtr::new(min, ptr))
+            .collect();
+
+        let capacity = data.len() / FANOUT + 2;
+        let nodes = Buffer::new_on_disk(capacity, path_with_extension(path.as_ref(), "bl"))?;
+
+        Ok(Self::build_internal(&data, nodes))
+    }
+
+    fn load(path: impl AsRef<Path>) -> Result<Self>
+    where
+        Self: Sized,
+    {
+        let nodes = Buffer::load_from_disk(path_with_extension(path.as_ref(), "bl"))?;
+
+        Ok(Self { nodes })
+    }
+}
+
+impl<K: Key, const FANOUT: usize> BTreeLayer<K, FANOUT> {
+    fn build_internal(
+        data: &[KeyPtr<K, usize>],
+        mut nodes: Buffer<BTreeNode<K, usize, FANOUT>>,
+    ) -> Self {
+        // Always add extra padding node
+        nodes[0] = BTreeNode::empty();
+
+        let mut index = 1;
+        let mut start = 0;
+
+        // TODO: replace with slice
+        while start < data.len() {
+            let end = (start + FANOUT).min(data.len());
+            nodes[index] = BTreeNode::empty();
+
+            for &key_ptr in &data[start..end] {
+                nodes[index].push(key_ptr);
+            }
+
+            start = end;
+            index += 1;
+        }
+
+        nodes.shrink(index);
+
+        Self { nodes }
     }
 }
