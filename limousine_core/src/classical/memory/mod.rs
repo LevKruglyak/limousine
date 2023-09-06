c3afc1f03adadcea39c1a8d22016e312799451f1
LevKruglyak <lev.kruglyak2014@gmail.com>
2023-09-06 14:09:28 -0400

    backup

--- limousine_core/src/classical/memory/mod.rs
+++ limousine_core/src/classical/memory/mod.rs
@@ -12,7 +12,7 @@ mod layer;
 use crate::component::*;
 use crate::kv::StaticBounded;
 use layer::*;
-use std::ops::RangeBounds;
+use std::ops::{Bound, RangeBounds};
 
 // -------------------------------------------------------
 //                  Internal Component
@@ -39,8 +39,12 @@ where
 
     type Iter<'n> = <MemoryBTreeLayer<K, B::Address, FANOUT> as NodeLayer<K>>::Iter<'n>;
 
-    fn range<'n>(&'n self, range: impl RangeBounds<Self::Address>) -> Self::Iter<'n> {
-        self.inner.range(range)
+    fn range<'n>(
+        &'n self,
+        start: Bound<Self::Address>,
+        end: Bound<Self::Address>,
+    ) -> Self::Iter<'n> {
+        self.inner.range(start, end)
     }
 
     fn full_range<'n>(&'n self) -> Self::Iter<'n> {
@@ -53,7 +57,7 @@ impl<K, B: NodeLayer<K>, const FANOUT: usize> InternalComponent<K, B>
 where
     K: StaticBounded,
 {
-    fn search(&self, ptr: Self::Address, key: &K) -> B::Address {
+    fn search(&self, _: &B, ptr: Self::Address, key: &K) -> B::Address {
         let node = unsafe { ptr.as_ref() };
 
         node.inner.search_lub(key).clone()
@@ -61,18 +65,19 @@ where
 
     fn insert<'n>(
         &'n mut self,
+        base: &B,
         ptr: Self::Address,
-        prop: PropogateInsert<'_, K, B>,
-    ) -> Option<PropogateInsert<'n, K, Self>> {
+        prop: PropogateInsert<K, B>,
+    ) -> Option<PropogateInsert<K, Self>> {
         match prop {
             PropogateInsert::Single(key, address) => self
                 .inner
                 .insert(key, address, ptr)
                 .map(|(key, address)| PropogateInsert::Single(key, address)),
-            PropogateInsert::Rebuild(base) => {
+            PropogateInsert::Rebuild => {
                 self.inner.fill(base.full_range());
 
-                Some(PropogateInsert::Rebuild(self))
+                Some(PropogateInsert::Rebuild)
             }
         }
     }
@@ -125,8 +130,12 @@ where
 
     type Iter<'n> = <MemoryBTreeLayer<K, V, FANOUT> as NodeLayer<K>>::Iter<'n>;
 
-    fn range<'n>(&'n self, range: impl RangeBounds<Self::Address>) -> Self::Iter<'n> {
-        self.inner.range(range)
+    fn range<'n>(
+        &'n self,
+        start: Bound<Self::Address>,
+        end: Bound<Self::Address>,
+    ) -> Self::Iter<'n> {
+        self.inner.range(start, end)
     }
 
     fn full_range<'n>(&'n self) -> Self::Iter<'n> {
@@ -144,7 +153,7 @@ where
         ptr: Self::Address,
         key: K,
         value: V,
-    ) -> Option<PropogateInsert<'n, K, Self>> {
+    ) -> Option<PropogateInsert<K, Self>> {
         if let Some((key, address)) = self.inner.insert(key, value, ptr) {
             Some(PropogateInsert::Single(key, address))
         } else {
--- limousine_derive/src/lib.rs
+++ limousine_derive/src/lib.rs
@@ -6,288 +6,153 @@
 //! crate.
 
 #![deny(missing_docs)]
-use crate::layer_type::LayerType;
-use proc_macro2::{Ident, Span};
-use quote::{format_ident, quote, ToTokens};
-use syn::{
-    braced, parse,
-    parse::{Parse, ParseStream},
-    parse_macro_input, Arm, Token,
-};
+mod component;
+mod layout;
+mod util;
 
-mod layer_type;
+use layout::IndexLayout;
+use proc_macro2::{Span, TokenStream};
+use quote::quote;
+use syn::Ident;
 
-/// Macro to materialize an immutable hybrid index structure. To materialize a hybrid index,
-/// a name and a layout is required, for example:
+/// Macro to materialize an hybrid index structure. To use, specify
+/// a name for the generated structure and a layout. The layout consists of
+/// a list of layer types, ordered from highest to lowest; for example:
 ///
 /// ```ignore
 /// // Note: this is just a syntax example, this macro needs to be called
 /// // from the [`limousine_engine`](https://crates.io/crates/limousine_engine) crate.
 ///
-/// create_immutable_hybrid_index! {
+/// create_hybrid_index! {
 ///     name: MyHybridIndex,
 ///     layout: {
-///         0 | 1 => btree(32),
-///         _ => pgm(8),
+///         pgm(4),
+///         pgm(4),
+///         btree(32),          // base layer
 ///     }
 /// }
 /// ```
-/// The name is required to be some unique identifier which is not defined anywhere elsewhere in
-/// the scope. The layout follows the Rust match expression syntax, where each arm follows the
-/// format:
+/// Optionally, we can also specify the top component, which is allowed to grow vertically
+/// indefinitely. By default, this is set to the ```BTreeMap``` structure from the Rust standard
+/// library, however alternative implmentations based LSM and LSH trees are also provided.
 ///
-/// ```ignore
-/// [usize match body] => [layer_type](param1, param2, ...)
-/// ```
+/// TODO: example
 ///
-/// The supported layer types are:
+/// The supported component types in the layout are:
 ///
 /// 1. **btree(fanout: usize)**
-/// 2. **pgm(epsilon: usize)**
+/// 2. **disk_btree(fanout: usize)**
+/// 3. **pgm(epsilon: usize)**
 ///
-/// The macro will generate a structure with the given name, alongside an implementation of the
-/// `ImmutableIndex` trait.
+/// Note that not all layouts are valid; for instance trying to place a disk layer over an
+/// in-memory layer will result in an error. These rules are enforced automatically by the macro.
+/// The macro will generate a structure with the provided name, alongside an implementation of the
+/// `Index` trait.
 #[proc_macro]
-pub fn create_immutable_hybrid_index(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    // Generate documentation
-    let input_string = input.to_string();
-    let mut documentation = quote! {
-        #[doc = "Immutable hybrid index materialized by layout:"]
-        #[doc = ""]
-    };
-
-    for line in input_string.lines() {
-        let line = format!(
-            "```rust
-        {{
-        {line}
-        }}
-        ```"
-        );
-        documentation.extend(quote!(#[doc = #line]));
-    }
-
+pub fn create_hybrid_index(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     // Parse hybrid index description
-    let desc = parse_macro_input!(input as HybridIndexDescription);
-    let name = Ident::new(&desc.name, Span::mixed_site());
+    let layout = syn::parse_macro_input!(input as IndexLayout);
+    let name = layout.name();
 
-    let layer_name = Ident::new(
-        &format!("__{}Layer", &desc.name).to_string(),
-        Span::call_site(),
+    let mod_name = proc_macro2::Ident::new(
+        format!("__implmentation_{}", name.to_string().to_lowercase()).as_str(),
+        proc_macro2::Span::call_site(),
     );
 
-    // Materialize layer type
-    let mut variants = Vec::new();
-    for (i, layer) in desc.layer_types.iter().enumerate() {
-        let variant_name = format_ident!("L{}", i);
-        variants.push(quote! { #variant_name(#layer) });
-    }
+    let (alias_body, alias) = create_type_aliases(&layout);
+    let (index_body, index_fields) = create_index_struct(&layout, &alias);
 
-    // Materialize implementation for layer type
-    let mut len_arms = Vec::new();
-    let mut search_arms = Vec::new();
-    let mut key_iter_arms = Vec::new();
+    let mut implementation = proc_macro2::TokenStream::new();
+    implementation.extend(quote! {
+        pub mod #mod_name {
+            use ::limousine_engine::private::*;
 
-    for (i, _) in desc.layer_types.iter().enumerate() {
-        let variant_name = format_ident!("L{}", i);
+            #(#alias_body)*
 
-        len_arms.push(quote!(crate::#layer_name::#variant_name(_internal) => _internal.len()));
-        search_arms.push(
-            quote!(crate::#layer_name::#variant_name(_internal) => _internal.search(key, range)),
-        );
-        key_iter_arms.push(quote!(crate::#layer_name::#variant_name(_internal) => Box::new(_internal.nodes().iter().map(|x| *x.borrow()))));
-    }
-
-    let mut build_arms = Vec::new();
-    let mut build_on_disk_arms = Vec::new();
-    let mut load_arms = Vec::new();
-
-    for (arm, index) in desc.layout.iter().cloned() {
-        let layer_base = desc.layer_types[index].to_base();
-        let layout_variant_name = format_ident!("L{}", index);
-
-        let mut build_arm = arm.clone();
-        build_arm.body = Box::new(
-            syn::parse2(quote!(crate::#layer_name::#layout_variant_name(#layer_base::build(base))))
-                .expect(""),
-        );
-        build_arms.push(build_arm.to_token_stream());
-
-        let mut build_on_disk_arm = arm.clone();
-        build_on_disk_arm.body = Box::new(
-            syn::parse2(quote!(Ok(crate::#layer_name::#layout_variant_name(#layer_base::build_on_disk(base, path)?))))
-                .expect(""),
-        );
-        build_on_disk_arms.push(build_on_disk_arm.to_token_stream());
-
-        let mut load_arm = arm.clone();
-        load_arm.body = Box::new(
-            syn::parse2(
-                quote!(Ok(crate::#layer_name::#layout_variant_name(#layer_base::load(path)?))),
-            )
-            .expect(""),
-        );
-
-        load_arms.push(load_arm.to_token_stream());
-    }
-
-    // Layer implementation
-    let mut layer = quote! {
-        pub enum #layer_name<K: ::limousine_engine::private::Key> {
-            #(#variants),*
+            #index_body
         }
 
-        impl<K: ::limousine_engine::private::Key> ::limousine_engine::private::HybridLayer<K> for #layer_name<K> {
-            fn len(&self) -> usize {
-                use ::limousine_engine::private::NodeLayer;
+        use #mod_name::#name;
+    });
 
-                match self {
-                    #(#len_arms,)*
-                }
-            }
+    implementation.into()
+}
 
-            fn search(&self, key: &K, range: ::limousine_engine::private::ApproxPos) -> ::limousine_engine::private::ApproxPos {
-                use ::limousine_engine::private::InternalLayer;
+fn create_type_aliases(layout: &IndexLayout) -> (Vec<TokenStream>, Vec<Ident>) {
+    let mut type_alias = Vec::new();
+    let mut type_alias_body = Vec::new();
 
-                match self {
-                    #(#search_arms,)*
-                }
-            }
+    // Add body as the first component
+    type_alias.push(Ident::new("Component0", Span::call_site()));
 
-            fn build(layer: usize, base: impl ExactSizeIterator<Item = K>) -> Self {
-                use ::limousine_engine::private::InternalLayerBuild;
+    let alias = type_alias[0].clone();
+    let body = layout.base.to_tokens();
+    type_alias_body.push(quote::quote! {
+        type #alias<K, V> = #body;
+    });
 
-                match layer {
-                    #(#build_arms)*
-                }
-            }
+    // Add internal components
+    for (mut index, component) in layout.internal.iter().rev().enumerate() {
+        index += 1;
 
-            fn build_on_disk(
-                layer: usize,
-                base: impl ExactSizeIterator<Item = K>,
-                path: impl AsRef<std::path::Path>,
-            ) -> ::limousine_engine::private::Result<Self>
-            where
-                Self: Sized {
-                use ::limousine_engine::private::InternalLayerBuild;
+        type_alias.push(Ident::new(
+            format!("Component{}", index).as_str(),
+            Span::call_site(),
+        ));
 
-                match layer {
-                    #(#build_on_disk_arms)*
-                }
-            }
+        let previous_alias = type_alias[index - 1].clone();
+        let body = component.to_tokens(quote! { #previous_alias<K, V> });
 
-            fn load(layer: usize, path: impl AsRef<std::path::Path>) -> ::limousine_engine::private::Result<Self>
-            where
-                Self: Sized {
-                use ::limousine_engine::private::InternalLayerBuild;
+        let alias = type_alias[index].clone();
+        type_alias_body.push(quote! {
+            type #alias<K, V> = #body;
+        });
+    }
 
-                match layer {
-                    #(#load_arms)*
-                }
-            }
+    // Add top component
+    let index = layout.internal.len() + 1;
+    type_alias.push(Ident::new(
+        format!("Component{}", index).as_str(),
+        Span::call_site(),
+    ));
 
-            fn key_iter<'e>(&'e self) -> Box<dyn ExactSizeIterator<Item = K> + 'e> {
-                use ::limousine_engine::private::NodeLayer;
-                use std::borrow::Borrow;
+    let previous_alias = type_alias[index - 1].clone();
+    let body = layout.top.to_tokens(quote! { #previous_alias<K, V> });
 
-                match self {
-                    #(#key_iter_arms,)*
-                }
-            }
+    let alias = type_alias[index].clone();
+    type_alias_body.push(quote! {
+        type #alias<K, V> = #body;
+    });
+
+    (type_alias_body, type_alias)
+}
+
+fn create_index_struct(layout: &IndexLayout, alias: &Vec<Ident>) -> (TokenStream, Vec<Ident>) {
+    let name = layout.name();
+
+    // Create fields
+    let mut fields = Vec::new();
+    for component in alias.iter() {
+        fields.push(Ident::new(
+            component.to_string().to_lowercase().as_str(),
+            Span::call_site(),
+        ));
+    }
+
+    // Create field definitions
+    let mut field_bodies = Vec::new();
+    for (index, component) in alias.iter().enumerate() {
+        let field = fields[index].clone();
+        field_bodies.push(quote! {
+            #field: #component<K, V>,
+        });
+    }
+
+    let body = quote! {
+        pub struct #name<K: Key, V: Value> {
+            #(#field_bodies)*
         }
     };
 
-    // Index implementation
-    let index = quote! {
-        #documentation
-        #[allow(unused_import)]
-        pub struct #name<K: ::limousine_engine::private::Key, V: ::limousine_engine::private::Value>(::limousine_engine::private::HybridIndex<K, V, #layer_name<K>>);
-
-        impl<K: ::limousine_engine::private::Key, V: ::limousine_engine::private::Value> ::limousine_engine::private::ImmutableIndex<K, V> for #name<K, V> {
-            fn build_in_memory(base: impl ExactSizeIterator<Item = (K, V)>) -> Self {
-                Self(::limousine_engine::private::HybridIndex::build_in_memory(base))
-            }
-
-            fn build_on_disk(
-                base: impl ExactSizeIterator<Item = (K, V)>,
-                path: impl AsRef<::std::path::Path>,
-                threshold: usize,
-            ) -> ::limousine_engine::private::Result<Self> {
-                Ok(Self(::limousine_engine::private::HybridIndex::build_on_disk(base, path, threshold)?))
-            }
-
-            fn load(path: impl AsRef<::std::path::Path>, threshold: usize) -> ::limousine_engine::private::Result<Self> {
-                Ok(Self(::limousine_engine::private::HybridIndex::load(path, threshold)?))
-            }
-
-            fn lookup(&self, key: &K) -> Option<V> {
-                self.0.lookup(key)
-            }
-
-            fn range(&self, low: &K, high: &K) -> Self::RangeIterator<'_> {
-                self.0.range(low, high)
-            }
-
-            type RangeIterator<'e> = ::limousine_engine::private::HybridIndexRangeIterator<'e, K, V>;
-        }
-    };
-
-    layer.extend(index);
-    layer.into()
-}
-
-struct HybridIndexDescription {
-    name: String,
-    layer_types: Vec<LayerType>,
-    layout: Vec<(Arm, usize)>,
-}
-
-impl Parse for HybridIndexDescription {
-    fn parse(input: ParseStream) -> syn::Result<Self> {
-        let name: Ident = input.parse()?;
-        assert!(name.to_string() == "name".to_string(), "missing name!");
-
-        let _: Token![:] = input.parse()?;
-        let index_name: Ident = input.parse()?;
-        let _: Token![,] = input.parse()?;
-
-        let layout: Ident = input.parse()?;
-        assert!(
-            layout.to_string() == "layout".to_string(),
-            "missing layout!"
-        );
-        let _: Token![:] = input.parse()?;
-        let content;
-        braced!(content in input);
-        let mut layout = Vec::new();
-        let mut layer_types = Vec::new();
-
-        while !content.is_empty() {
-            let arm: Arm = content.parse()?;
-            let _: Option<Token![,]> = content.parse()?;
-            let body = arm.body.to_token_stream();
-            let layer_type: LayerType = parse(body.into())?;
-
-            if !layer_types.contains(&layer_type) {
-                layer_types.push(layer_type.clone());
-            }
-
-            let layer_index = layer_types
-                .iter()
-                .enumerate()
-                .find(|&(_, item)| *item == layer_type)
-                .unwrap()
-                .0;
-
-            layout.push((arm, layer_index));
-        }
-
-        let _: Option<Token![,]> = content.parse()?;
-
-        Ok(Self {
-            name: index_name.to_string(),
-            layer_types,
-            layout,
-        })
-    }
+    (body, fields)
 }
