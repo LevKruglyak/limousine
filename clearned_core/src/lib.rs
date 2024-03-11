ae08dfb962f43492fbfaa2acdcfd9642cbd80475
LevKruglyak <lev.kruglyak2014@gmail.com>
2024-03-11 15:38:47 -0400

    move stuff over

--- clearned_core/src/lib.rs
+++ clearned_core/src/lib.rs
@@ -1,1 +1,97 @@
+use bytemuck::Pod;
+use num::PrimInt;
+use std::{borrow::Borrow, fmt::Debug, path::Path};
+use trait_set::trait_set;
 
+pub mod classical;
+pub mod learned;
+mod search;
+
+#[cfg(feature = "metrics")]
+pub mod metrics;
+
+/// Generic error type (to avoid a dependency on anyhow)
+pub type Result<T> = core::result::Result<T, Box<dyn std::error::Error>>;
+
+// Until `trait_alias` is stabilized, we have to use a macro
+trait_set! {
+    /// General value type, thread-safe and POD
+    pub trait Value = Send + Sync + Pod + Debug;
+
+    /// General key type, thread safe, POD, and primitive integer type
+    pub trait Key = Value + PrimInt;
+}
+
+/// Represents an immutable tree index over a sorted slice of entries
+pub trait ImmutableIndex<K: Key, V: Value>: Sized {
+    /// Build an index over the given data in memory
+    /// This method assumes the data is sorted, and without key repetitions
+    fn build(base: impl ExactSizeIterator<Item = (K, V)>) -> Self;
+
+    /// Build an index over the given data, persisting to disk
+    /// This method assumes the data is sorted, and without key repetitions
+    fn build_disk(
+        base: impl ExactSizeIterator<Item = (K, V)>,
+        path: impl AsRef<Path>,
+    ) -> Result<Self>;
+
+    /// Load an index from memory, rebuilding layers which weren't persisted
+    fn load(path: impl AsRef<Path>) -> Result<Self>;
+
+    /// Returns `Some(entry)` if the is an entry with key `key`, otherwise `None`
+    fn lookup(&self, key: &K) -> Option<V>;
+
+    /// Returns an iterator which iterates the entries between `low` and `high`, inclusive
+    fn range(&self, low: &K, high: &K) -> Self::RangeIterator<'_>;
+
+    /// Return the size of the index in bytes
+    fn size(&self) -> usize;
+
+    type RangeIterator<'e>: Iterator<Item = (&'e K, &'e V)>
+    where
+        Self: 'e;
+}
+
+/// Some layer of nodes on top of which an index layer can be built.
+pub trait NodeLayer<K: Key> {
+    type Node: Borrow<K>;
+
+    /// Returns a slice of keys, each of which represent the
+    /// minimum keys of the underlying node.
+    fn nodes(&self) -> &[Self::Node];
+}
+
+/// A range of potential locations for the results of a query.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct ApproxPos {
+    lo: usize,
+    hi: usize,
+}
+
+/// An internal layer of nodes which can search using an `ApproxPos`
+/// to return some other `ApproxPos`. Not all `InternalLayers` need
+/// the full flexibility of the `ApproxPos` generality,
+/// but since we must support learned index layers, we need this generality.
+pub trait InternalLayer<K: Key>: NodeLayer<K> {
+    fn search(&self, key: &K, range: ApproxPos) -> ApproxPos;
+}
+
+/// A set of static methods for building a particular type of internal layer.
+pub trait InternalLayerBuild<K: Key>: NodeLayer<K> {
+    /// Build an index layer over the given data, storing in memory
+    /// Assumes the data is sorted, and without key repetitions
+    fn build(base: Vec<K>) -> Self
+    where
+        Self: Sized;
+
+    /// Build an index layer over the given data, persisting to disk
+    /// Assumes the data is sorted, and without key repetitions
+    fn build_on_disk(base: Vec<K>, path: impl AsRef<Path>) -> crate::Result<Self>
+    where
+        Self: Sized;
+
+    /// Load an index from memory, rebuilding layers which weren't persisted
+    fn load(path: impl AsRef<Path>) -> crate::Result<Self>
+    where
+        Self: Sized;
+}
---
+++ clearned_core/src/search.rs
@@ -0,0 +1,148 @@
+use std::borrow::Borrow;
+
+/// Returns the `upper bound` index from a search result
+pub fn upper_bound(search: Result<usize, usize>, cap: usize) -> usize {
+    match search {
+        Ok(index) => index,
+        Err(index) => index.min(cap),
+    }
+}
+
+/// Returns the `lower bound` index from a search result
+pub fn lower_bound(search: Result<usize, usize>) -> usize {
+    match search {
+        Ok(index) => index,
+        Err(index) => index.saturating_sub(1),
+    }
+}
+
+/// Some algorithm for searching a slice, e.g. Binary or Linear
+pub trait Search {
+    /// Same as calling `search_by_key` when `T` == `K`.
+    fn search<T: Ord + Copy>(slice: &[T], x: &T) -> Result<usize, usize> {
+        Self::search_by_key(slice, x)
+    }
+
+    /// If the value is found in the slice, will return `Ok(index)` with the
+    /// index of the found value. If it is not found, will return `Err(index)`
+    /// with the index of where the value should be inserted to maintain the
+    /// sorted order.
+    ///
+    /// This method assumes that the given slice is sorted, and contains no duplicates
+    fn search_by_key<K: Ord + Copy, T: Borrow<K>>(slice: &[T], x: &K) -> Result<usize, usize>;
+
+    /// Same as calling `search`, but adjusts for the case when
+    /// slice's index 0 is actually offset in some larger slice
+    fn search_with_offset<T: Ord + Copy>(
+        slice: &[T],
+        x: &T,
+        offset: usize,
+    ) -> Result<usize, usize> {
+        Self::search_by_key(slice, x)
+            .map(|x| x + offset)
+            .map_err(|x| x + offset)
+    }
+
+    /// Same as calling `search_by_key`, but adjusts for the case
+    /// when the slice's index 0 is actually offset in some larger slice
+    fn search_by_key_with_offset<K: Ord + Copy, T: Borrow<K>>(
+        slice: &[T],
+        x: &K,
+        offset: usize,
+    ) -> Result<usize, usize> {
+        Self::search_by_key(slice, x)
+            .map(|x| x + offset)
+            .map_err(|x| x + offset)
+    }
+}
+
+/// Performs a binary search on a slice, with computational complexity `O(log n)`
+/// However, for small searches, a linear search may be faster.
+pub struct BinarySearch;
+
+impl Search for BinarySearch {
+    fn search_by_key<K: Ord + Copy, T: Borrow<K>>(slice: &[T], x: &K) -> Result<usize, usize> {
+        slice.binary_search_by_key(x, |x| *x.borrow())
+    }
+}
+
+/// Performs a simple linear search on a slice, with computational complexity `O(n)`
+pub struct LinearSearch;
+
+impl Search for LinearSearch {
+    fn search_by_key<K: Ord + Copy, T: Borrow<K>>(slice: &[T], x: &K) -> Result<usize, usize> {
+        let mut index = 0;
+        let size = slice.len();
+
+        while index < size && unsafe { slice.get_unchecked(index).borrow() } < x {
+            index += 1;
+        }
+
+        if index >= size {
+            Err(size)
+        } else if unsafe { slice.get_unchecked(index).borrow() } == x {
+            Ok(index)
+        } else {
+            Err(index)
+        }
+    }
+}
+
+/// If a slice has less than `LINEAR_SEARCH_THRESHOLD` bytes, use a linear search
+/// TODO: experimentally determine
+const LINEAR_SEARCH_THRESHOLD: usize = 8 * 64;
+
+/// Decides whether to use a linear or binary search based on some experimentally determined
+/// threshold on the size of the given slice.
+pub struct OptimalSearch;
+
+impl Search for OptimalSearch {
+    fn search_by_key<K: Ord + Copy, T: Borrow<K>>(slice: &[T], x: &K) -> Result<usize, usize> {
+        if slice.len() * std::mem::size_of::<T>() > LINEAR_SEARCH_THRESHOLD {
+            BinarySearch::search_by_key(slice, x)
+        } else {
+            LinearSearch::search_by_key(slice, x)
+        }
+    }
+}
+
+#[cfg(test)]
+mod search_tests {
+    use super::*;
+
+    #[test]
+    fn test_upper_bound() {
+        assert_eq!(upper_bound(Ok(3), 5), 3);
+        assert_eq!(upper_bound(Err(3), 5), 3);
+        assert_eq!(upper_bound(Err(7), 5), 5);
+    }
+
+    #[test]
+    fn test_lower_bound() {
+        assert_eq!(lower_bound(Ok(3)), 3);
+        assert_eq!(lower_bound(Err(3)), 2);
+        assert_eq!(lower_bound(Err(0)), 0);
+    }
+
+    #[test]
+    fn linear_search() {
+        let array = [1, 2, 3, 4, 7, 10, 24, 55, 56, 57, 100];
+        for i in -10..110 {
+            assert_eq!(
+                BinarySearch::search(&array[..], &i),
+                LinearSearch::search(&array[..], &i)
+            );
+        }
+    }
+
+    #[test]
+    fn optimal_search() {
+        let array = [1, 2, 3, 4, 7, 10, 24, 55, 56, 57, 100];
+        for i in -10..110 {
+            assert_eq!(
+                BinarySearch::search(&array[..], &i),
+                OptimalSearch::search(&array[..], &i)
+            );
+        }
+    }
+}
