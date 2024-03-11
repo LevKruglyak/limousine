e49dedb26f4e5c2608d12d4da92d60d422c7f01a
LevKruglyak <lev.kruglyak2014@gmail.com>
2024-03-11 15:39:38 -0400

    fix test

--- limousine_engine/src/lib.rs
+++ limousine_engine/src/lib.rs
@@ -1,9 +1,9 @@
 //! ***limousine_engine*** provides a procedural macro to automatically generate an (immutable) hybrid index design:
 //!
-//! ```rust
+//! ```ignore
 //! use limousine_engine::prelude::*;
 //!
-//! limousine_macros::create_immutable_hybrid_index! {
+//! create_immutable_hybrid_index! {
 //!     name: ExampleHybridIndex,
 //!     layout: {
 //!         0 | 1 => btree(16),
@@ -14,17 +14,17 @@
 //!
 //! To generate a design, we provide a name for the structure, and a layout description, which resembles the syntax of a Rust match expression. In this example, the first two layers are BTree layers with a fanout of 16, and the rest of the layers are PGM layers with an epsilon parameter of 4. All of this is parsed and generated into a static implementation at compile time by the procedural macro. We can also generate efficient pure designs using this approach:
 //!
-//! ```rust
+//! ```ignore
 //! use limousine_engine::prelude::*;
 //!
-//! limousine_marcos::create_immutable_hybrid_index! {
+//! create_immutable_hybrid_index! {
 //!     name: BTreeIndex,
 //!     layout: {
 //!         _ => btree(16),
 //!     }
 //! }
 //!
-//! limousine_marcos::create_immutable_hybrid_index! {
+//! create_immutable_hybrid_index! {
 //!     name: PGMIndex,
 //!     layout: {
 //!         _ => pgm(4),
@@ -34,7 +34,7 @@
 //!
 //! We can then use these generated structs to perform queries:
 //!
-//! ```rust
+//! ```ignore
 //! // Load the first two layer of the index from memory
 //! let index = ExampleHybridIndex::<i32, i32>::load("path_to_index", 2)?;
 //!
@@ -48,11 +48,7 @@
 /// Include this at the top of the file when materializing a hybrid index or using a hybrid index.
 pub mod prelude {
     pub use limousine_core::ImmutableIndex;
-
-    /// Macros for materializing hybrid indexes
-    pub mod limousine_macros {
-        pub use limousine_derive::create_immutable_hybrid_index;
-    }
+    pub use limousine_derive::create_immutable_hybrid_index;
 }
 
 #[doc(hidden)]
