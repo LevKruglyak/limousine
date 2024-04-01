8502a43d7585a05f584a582f161df502f42b825d
LevKruglyak <lev.kruglyak2014@gmail.com>
2024-04-01 15:35:34 -0400

    add non-persisted index tests

--- limousine_tests/src/lib.rs
+++ limousine_tests/src/lib.rs
@@ -82,6 +82,40 @@ mod tests {
         }
     }
 
+    fn test_index<I: Index<K, V>>() {
+        let mut rng = thread_rng();
+        let key_dist = Uniform::new(K::MIN, K::MAX);
+        let value_dist = Uniform::new(V::MIN, V::MAX);
+
+        let num = 20_000;
+        let keys: Vec<K> = (&mut rng)
+            .sample_iter(key_dist)
+            .filter(|&x| x < 0 as K || x > 10_000 as K) // we want to test for false positives as
+            // well
+            .take(num)
+            .collect();
+
+        let values: Vec<V> = (&mut rng).sample_iter(value_dist).take(num).collect();
+
+        {
+            let mut index = I::empty();
+
+            // Test inserts
+            for i in 0..num {
+                index.insert(keys[i], values[i]);
+            }
+
+            // Test searches
+            for i in 0..num {
+                assert_eq!(index.search(keys[i]), Some(values[i]));
+            }
+
+            for key in 0..10_000 {
+                assert_eq!(index.search(key as K), None);
+            }
+        }
+    }
+
     #[test]
     fn test_persisted_index_1() {
         create_hybrid_index! {
@@ -187,5 +221,112 @@ mod tests {
         }
 
         test_persisted_index::<Index<K, V>>();
+    }
+
+    #[test]
+    fn test_index_1() {
+        create_hybrid_index! {
+            name: Index1,
+            layout: [
+                btree_top(),
+                btree(fanout = 64),
+            ]
+        }
+
+        test_index::<Index1<K, V>>();
+    }
+
+    #[test]
+    fn test_index_2() {
+        create_hybrid_index! {
+            name: Index1,
+            layout: [
+                btree_top(),
+                btree(fanout = 32),
+            ]
+        }
+
+        test_index::<Index1<K, V>>();
+    }
+
+    #[test]
+    fn test_index_3() {
+        create_hybrid_index! {
+            name: Index1,
+            layout: [
+                btree_top(),
+                btree(fanout = 32),
+                btree(fanout = 32),
+            ]
+        }
+
+        test_index::<Index1<K, V>>();
+    }
+
+    #[test]
+    fn test_index_4() {
+        create_hybrid_index! {
+            name: Index1,
+            layout: [
+                btree_top(),
+                btree(fanout = 8),
+                btree(fanout = 8),
+                btree(fanout = 32),
+            ]
+        }
+
+        test_index::<Index1<K, V>>();
+    }
+
+    #[test]
+    fn test_index_5() {
+        create_hybrid_index! {
+            name: Index1,
+            layout: [
+                btree_top(),
+                btree(fanout = 8),
+                btree(fanout = 8),
+                btree(fanout = 8),
+                btree(fanout = 32),
+            ]
+        }
+
+        test_index::<Index1<K, V>>();
+    }
+
+    #[test]
+    fn test_index_6() {
+        create_hybrid_index! {
+            name: Index1,
+            layout: [
+                btree_top(),
+                btree(fanout = 8),
+                btree(fanout = 8),
+                btree(fanout = 8),
+                btree(fanout = 8),
+                btree(fanout = 8),
+                btree(fanout = 32),
+            ]
+        }
+
+        test_index::<Index1<K, V>>();
+    }
+
+    #[test]
+    fn test_index_7() {
+        create_hybrid_index! {
+            name: Index1,
+            layout: [
+                btree_top(),
+                btree(fanout = 8),
+                btree(fanout = 8),
+                btree(fanout = 8),
+                btree(fanout = 8),
+                btree(fanout = 8),
+                btree(fanout = 8),
+            ]
+        }
+
+        test_index::<Index1<K, V>>();
     }
 }
